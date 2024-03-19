7d89290f7aaa3e554a740cec753e7fba55a1c566
vitayushchyk <vitay2715@gmail.com>
2024-03-19 18:39:32 +0200

    CSA-56: implemented user model and migrations

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=256, blank=False, unique=True, )
+    password = models.CharField(max_length=256, blank=False, unique=True, )
+    avatar = models.ImageField(upload_to='')  # Where
+    last_name = None
+    first_name = None
+    is_active = True
+    is_staff = False
+
+    REQUIRED_FIELDS = ['email', 'password']
+
+
+class UserStatistics(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_roadmaps = models.IntegerField()
+    completed_roadmaps = models.IntegerField()
+    in_progress_roadmaps = models.IntegerField()
