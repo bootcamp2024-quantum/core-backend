750e6eee2419022ffaa4e9acc3eacbc974009942
Serhii Sliadniev <serhii@bridgewise.com>
2024-03-20 23:24:24 +0200

    CSA-40: minor changes

--- user/models.py
+++ user/models.py
@@ -1,11 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from user.managers import CustomUserManager
 
 
 class User(AbstractUser):
     first_name = models.CharField(max_length=30, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
-
     avatar = models.ImageField(upload_to='user_avatar', blank=True)
 
     REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
