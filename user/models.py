dfd48eb8e9d8ddb8ab612ec60d8508a2748a4bdb
vitayushchyk <vitay2715@gmail.com>
2024-03-21 20:34:25 +0200

    CSA-44:  fix comments

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ from user.managers import CustomUserManager
 class User(AbstractUser):
     first_name = models.CharField(max_length=30, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
-    avatar = models.ImageField(upload_to='media/user_avatar', blank=True)
+    avatar = models.ImageField(upload_to='user_avatar', blank=True)
 
     REQUIRED_FIELDS = ['email']
 
