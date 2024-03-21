7999ac1dd9dfd2c3dd3b13e94f2c7ea7552fda2a
Vita Yushchyk <151214591+vitayushchyk@users.noreply.github.com>
2024-03-21 21:41:33 +0200

    CSA-44:  implemented endpoint to create a new user account (#10)
    * CSA-56: implemented user model and migrations
    
    * CSA-56: update user model and migrations
    
    * CSA-56: deleted in password `unique=True`
    
    * CSA-44:  endpoint to create a new user account
    
    * CSA-44:  implemented endpoint to create a new user account
    
    * CSA-44:  fix comments
    
    ---------
    
    Co-authored-by: vitayushchyk <vitay2715@gmail.com>

--- career_skill_atlas/config/base_settings.py
+++ career_skill_atlas/config/base_settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user',
 ]
 
@@ -128,3 +129,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.User'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/media'
---
+++ user/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    repeat_password = serializers.CharField(write_only=True)
+    avatar = serializers.ImageField(required=False, allow_null=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'repeat_password', 'avatar')
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        repeat_password = attrs.get('repeat_password')
+        if password != repeat_password:
+            raise serializers.ValidationError('Passwords do not match.')
+        attrs.pop('repeat_password', None)
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            validated_data['email'],
+            validated_data['password'],
+            username=validated_data['username'],
+            avatar=validated_data['avatar']
+        )
+        user.save()
+        return user
