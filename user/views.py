8b42886e42c9075fb1652a57cee11744f0ee80f4
Alfur <zbsrebyata@gmail.com>
2024-03-21 19:16:41 +0200

    CSA-54: fixed responses inconsistencies.

--- user/views.py
+++ user/views.py
@@ -18,10 +18,11 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         try:
             serializer.is_valid(raise_exception=True)
         except rf_exceptions.ValidationError as e:
-            msg = [f"Field '{key}' may not be blank." for key in e.detail.keys()]
+            msg = "\n".join([f"Field '{key}' may not be blank." for key in e.detail.keys()])
             return Response(data={"message": msg, "code": 400}, status=status.HTTP_400_BAD_REQUEST)
         except rf_jwt_exceptions.TokenError as e:
-            return Response(data={"message": e, "code": 500}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(data={"message": ", ".join(e.args), "code": 500},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         try:
             _ = serializer.validated_data["code"]
