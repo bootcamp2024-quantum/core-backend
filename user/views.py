548f45eb312fd1e41739eb5c91c3c5191faa1464
Alfur <zbsrebyata@gmail.com>
2024-03-21 21:32:09 +0200

    CSA-54: fixed responses inconsistencies.

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework_simplejwt import exceptions as rf_jwt_exceptions
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.exceptions import TokenError
 
 from .models import User
 from .serializers import UserSerializer, CustomTokenObtainPairSerializer
@@ -20,7 +21,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         except rf_exceptions.ValidationError as e:
             msg = "\n".join([f"Field '{key}' may not be blank." for key in e.detail.keys()])
             return Response(data={"message": msg, "code": 400}, status=status.HTTP_400_BAD_REQUEST)
-        except rf_jwt_exceptions.TokenError as e:
+        except TokenError as e:
             return Response(data={"message": ", ".join(e.args), "code": 500},
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
