43d2f0841d2f92daa10ee854668cc80b2b053713
Alfur <zbsrebyata@gmail.com>
2024-03-28 15:13:31 +0200

    CSA-71 Fixed fields naming in responses. For future: make sure you are always following the swagger instructions! Also, fixed tests for RUD views.

--- user/tests/test_user_delete_endpoint.py
+++ user/tests/test_user_delete_endpoint.py
@@ -8,7 +8,6 @@ class DeleteUserApiViewTests(APITestCase):
     users_url = "/api/users/"
 
     def setUp(self):
-        self.client = APIClient()
         self.user = User.objects.create(
             username="admin",
             email="admin@gmail.com",
@@ -24,11 +23,11 @@ class DeleteUserApiViewTests(APITestCase):
         )
 
     def test_delete_user(self):
-        response = self.client.delete(self.users_url + str(self.test_user.id))
+        response = self.client.delete(self.users_url + str(self.test_user.id) + "/")
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(User.objects.filter(username="TestSomeUserName").exists())
 
-    def test_delete_unexcisted_id_user(self):
-        response = self.client.delete(self.users_url + str(self.test_user.id + 1))
+    def test_delete_unexciting_user(self):
+        response = self.client.delete(self.users_url + str(self.test_user.id + 1) + "/")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- user/tests/test_user_put_endpoint.py
+++ user/tests/test_user_put_endpoint.py
@@ -8,15 +8,14 @@ from user.models import User
 
 # todo add tests for avatar field
 class PutUserApiViewTests(APITestCase):
-    users_url = "/api/users/"
-    user_request_data = {
-        "username": "SomeUserName",
-        "email": "someEmail@gmail.com",
-        "avatar": None,
-    }
 
     def setUp(self):
-        self.client = APIClient()
+        self.users_url = "/api/users/"
+        self.user_request_data = {
+            "name": "SomeUserName",
+            "email": "someEmail@gmail.com",
+            "avatar": None,
+        }
         self.user = User.objects.create(
             username="admin",
             email="admin@gmail.com",
@@ -33,12 +32,12 @@ class PutUserApiViewTests(APITestCase):
 
     def test_update_user(self):
 
-        url = self.users_url + str(self.test_user.id)
+        url = self.users_url + str(self.test_user.id) + "/"
 
         response = self.client.put(url, self.user_request_data, format="json")
         response_body = response.content.decode("utf-8")
         parsed_response = json.loads(response_body)
         user = User.objects.get(id=self.test_user.id)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(parsed_response["username"], user.username)
+        self.assertEqual(parsed_response["name"], user.username)
         self.assertEqual(parsed_response["email"], user.email)
